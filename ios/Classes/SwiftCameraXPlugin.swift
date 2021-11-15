1e644b9cdc1f509801b15538611abedd0d394862
iAMD <yanshouwang@outlook.com>
2021-11-15 18:11:43 +0800

    临时提交

--- ios/Classes/SwiftCameraXPlugin.swift
+++ ios/Classes/SwiftCameraXPlugin.swift
@@ -1,63 +1,149 @@
 import AVFoundation
 import Flutter
-import MLKitVision
-import MLKitBarcodeScanning
+import Messages
+
+let NAMESPACE = "yanshouwang.dev/camerax"
+
+typealias Command = Messages_Command
+typealias CommandCategory = Messages_CommandCategory
+typealias OpenArguments = Messages_OpenArguments
+typealias CameraSelector = Messages_CameraSelector
+typealias CameraFacing = Messages_CameraFacing
+typealias CameraValue = Messages_CameraValue
+typealias TextureValue = Messages_TextureValue
+typealias TorchValue = Messages_TorchValue
+typealias ZoomValue = Messages_ZoomValue
+typealias ImageProxy = Messages_ImageProxy
+typealias Event = Messages_Event
+typealias EventCategory = Messages_EventCategory
 
 public class SwiftCameraXPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, FlutterTexture, AVCaptureVideoDataOutputSampleBufferDelegate {
-    
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let instance = SwiftCameraXPlugin(registrar.textures())
-        
-        let method = FlutterMethodChannel(name: "yanshouwang.dev/camerax/method", binaryMessenger: registrar.messenger())
-        registrar.addMethodCallDelegate(instance, channel: method)
-        
-        let event = FlutterEventChannel(name: "yanshouwang.dev/camerax/event", binaryMessenger: registrar.messenger())
-        event.setStreamHandler(instance)
+        let instance = SwiftCameraXPlugin(registrar)
+        let binaryMessenger = registrar.messenger()
+        let methodChannel = FlutterMethodChannel(name: "\(NAMESPACE)/method", binaryMessenger: binaryMessenger)
+        let eventChannel = FlutterEventChannel(name: "\(NAMESPACE)/event", binaryMessenger: binaryMessenger)
+        registrar.addMethodCallDelegate(instance, channel: methodChannel)
+        eventChannel.setStreamHandler(instance)
     }
     
-    let registry: FlutterTextureRegistry
-    var sink: FlutterEventSink!
+    let textureRegistry: FlutterTextureRegistry
+    var eventSink: FlutterEventSink!
     var textureId: Int64!
     var captureSession: AVCaptureSession!
     var device: AVCaptureDevice!
     var latestBuffer: CVImageBuffer!
-    var analyzeMode: Int
-    var analyzing: Bool
     
-    init(_ registry: FlutterTextureRegistry) {
-        self.registry = registry
-        analyzeMode = 0
-        analyzing = false
+    init(_ registrar: FlutterPluginRegistrar) {
+        textureRegistry = registrar.textures()
         super.init()
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch call.method {
-        case "state":
-            stateNative(call, result)
-        case "request":
-            requestNative(call, result)
-        case "start":
-            startNative(call, result)
-        case "torch":
-            torchNative(call, result)
-        case "analyze":
-            analyzeNative(call, result)
-        case "stop":
-            stopNative(result)
+        let command = call.command
+        switch command.category {
+        case .open:
+            open(command.openArguments, result)
+        case .close:
+            close(command.key, result)
+        case .torch:
+            torch(command.key, command.torchState, result)
+        case .zoom:
+            zoom(command.key, command.zoomValue, result)
+        case .closeImageProxy:
+            closeImageProxy(command.key, result)
         default:
             result(FlutterMethodNotImplemented)
         }
     }
     
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        sink = events
+        eventSink = events
         return nil
     }
     
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        sink = nil
+        eventSink = nil
         return nil
+    }
+    
+    func open(_ openArguments: OpenArguments, _ result: @escaping FlutterResult) {
+        textureId = textureRegistry.register(self)
+        captureSession = AVCaptureSession()
+        let position = openArguments.selector.facing == .back ? AVCaptureDevice.Position.back : .front
+        if #available(iOS 10.0, *) {
+            device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: position).devices.first
+        } else {
+            device = AVCaptureDevice.devices(for: .video).filter({$0.position == position}).first
+        }
+        device.addObserver(self, forKeyPath: #keyPath(AVCaptureDevice.torchMode), options: .new, context: nil)
+        captureSession.beginConfiguration()
+        // Add device input.
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            captureSession.addInput(input)
+        } catch {
+            error.throwToFlutter(result)
+        }
+        // Add video output.
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+        captureSession.addOutput(videoOutput)
+        for connection in videoOutput.connections {
+            connection.videoOrientation = .portrait
+            if position == .front && connection.isVideoMirroringSupported {
+                connection.isVideoMirrored = true
+            }
+        }
+        captureSession.commitConfiguration()
+        captureSession.startRunning()
+        let demensions = CMVideoFormatDescriptionGetDimensions(device.activeFormat.formatDescription)
+        let width = Double(demensions.height)
+        let height = Double(demensions.width)
+        let size = ["width": width, "height": height]
+        let answer: [String : Any?] = ["textureId": textureId, "size": size, "torchable": device.hasTorch]
+        result(answer)
+    }
+    
+    func close(_ key:String, _ result: FlutterResult) {
+        captureSession.stopRunning()
+        for input in captureSession.inputs {
+            captureSession.removeInput(input)
+        }
+        for output in captureSession.outputs {
+            captureSession.removeOutput(output)
+        }
+        device.removeObserver(self, forKeyPath: #keyPath(AVCaptureDevice.torchMode))
+        textureRegistry.unregisterTexture(textureId)
+        
+        latestBuffer = nil
+        captureSession = nil
+        device = nil
+        textureId = nil
+        
+        result(nil)
+    }
+    
+    func torch(_ key:String, _ state: Bool, _ result: @escaping FlutterResult) {
+        do {
+            try device.lockForConfiguration()
+            device.torchMode = state ? .on : .off
+            device.unlockForConfiguration()
+            result(nil)
+        } catch {
+            error.throwToFlutter(result)
+        }
+    }
+    
+    func zoom(_ key: String, _ value: Double, _ result: @escaping FlutterResult) {
+        device.videoZoomFactor = value
+        result(nil)
+    }
+    
+    func closeImageProxy(_ key:String,_ result:FlutterResult) {
+        
     }
     
     public func copyPixelBuffer() -> Unmanaged<CVPixelBuffer>? {
@@ -70,7 +156,7 @@ public class SwiftCameraXPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, 
     public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
         latestBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
-        registry.textureFrameAvailable(textureId)
+        textureRegistry.textureFrameAvailable(textureId)
         
         switch analyzeMode {
         case 1: // barcode
@@ -111,91 +197,29 @@ public class SwiftCameraXPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, 
         AVCaptureDevice.requestAccess(for: .video, completionHandler: { result($0) })
     }
     
-    func startNative(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
-        textureId = registry.register(self)
-        captureSession = AVCaptureSession()
-        let position = call.arguments as! Int == 0 ? AVCaptureDevice.Position.front : .back
-        if #available(iOS 10.0, *) {
-            device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: position).devices.first
-        } else {
-            device = AVCaptureDevice.devices(for: .video).filter({$0.position == position}).first
-        }
-        device.addObserver(self, forKeyPath: #keyPath(AVCaptureDevice.torchMode), options: .new, context: nil)
-        captureSession.beginConfiguration()
-        // Add device input.
-        do {
-            let input = try AVCaptureDeviceInput(device: device)
-            captureSession.addInput(input)
-        } catch {
-            error.throwNative(result)
-        }
-        // Add video output.
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
-        videoOutput.alwaysDiscardsLateVideoFrames = true
-        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.main)
-        captureSession.addOutput(videoOutput)
-        for connection in videoOutput.connections {
-            connection.videoOrientation = .portrait
-            if position == .front && connection.isVideoMirroringSupported {
-                connection.isVideoMirrored = true
-            }
-        }
-        captureSession.commitConfiguration()
-        captureSession.startRunning()
-        let demensions = CMVideoFormatDescriptionGetDimensions(device.activeFormat.formatDescription)
-        let width = Double(demensions.height)
-        let height = Double(demensions.width)
-        let size = ["width": width, "height": height]
-        let answer: [String : Any?] = ["textureId": textureId, "size": size, "torchable": device.hasTorch]
-        result(answer)
-    }
-    
-    func torchNative(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
-        do {
-            try device.lockForConfiguration()
-            device.torchMode = call.arguments as! Int == 1 ? .on : .off
-            device.unlockForConfiguration()
-            result(nil)
-        } catch {
-            error.throwNative(result)
-        }
-    }
-    
-    func analyzeNative(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
-        analyzeMode = call.arguments as! Int
-        result(nil)
-    }
-    
-    func stopNative(_ result: FlutterResult) {
-        captureSession.stopRunning()
-        for input in captureSession.inputs {
-            captureSession.removeInput(input)
-        }
-        for output in captureSession.outputs {
-            captureSession.removeOutput(output)
-        }
-        device.removeObserver(self, forKeyPath: #keyPath(AVCaptureDevice.torchMode))
-        registry.unregisterTexture(textureId)
-        
-        analyzeMode = 0
-        latestBuffer = nil
-        captureSession = nil
-        device = nil
-        textureId = nil
-        
-        result(nil)
-    }
-    
     public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         switch keyPath {
         case "torchMode":
             // off = 0; on = 1; auto = 2;
             let state = change?[.newKey] as? Int
             let event: [String: Any?] = ["name": "torchState", "data": state]
-            sink?(event)
+            eventSink?(event)
         default:
             break
         }
+    }
+}
+
+extension FlutterMethodCall {
+    var command: Messages_Command {
+        let typedArguments = arguments as! FlutterStandardTypedData
+        return try! Messages_Command(serializedData: typedArguments.data)
+    }
+}
+
+extension Error {
+    func throwToFlutter(_ result: FlutterResult) {
+        let error = FlutterError(code: localizedDescription, message: nil, details: nil)
+        result(error)
     }
 }
--- ios/camerax.podspec
+++ ios/camerax.podspec
@@ -13,9 +13,9 @@ Pod::Spec.new do |s|
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.dependency 'GoogleMLKit/BarcodeScanning'
+  s.dependency 'SwiftProtobuf', '~> 1.0'
   s.static_framework = true
-  s.platform = :ios, '10.0'
+  s.platform = :ios, '8.0'
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
